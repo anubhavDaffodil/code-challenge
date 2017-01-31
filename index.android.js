0441c52bedd319568d3fb563ec9490b694dac213
Anubhav <anubhav.dhawan@daffodilsw.com>
2017-01-31 13:24:30 +0530

    more compact registry code

--- index.android.js
+++ index.android.js
@@ -1,13 +1,4 @@
-import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import App from './app';
 
-export default class sportdec extends Component {
-  render() {
-    return (
-      <App />
-    );
-  }
-}
-
-AppRegistry.registerComponent('sportdec', () => sportdec);
+AppRegistry.registerComponent('sportdec', () => App);
