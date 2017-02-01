25bce5863292f45edf15b1bc89081f58845d8f75
Anubhav <anubhav.dhawan@daffodilsw.com>
2017-02-01 16:16:35 +0530

    MatchDetails header part

--- app/routes/MatchDetail/MatchDetail.js
+++ app/routes/MatchDetail/MatchDetail.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import styles from './styles';
 import Icon from '../../components/Icon';
+import TeamCard from '../../components/TeamCard';
 
 const MatchDetail = props => {
 	return (
 		<View style={styles.container}>
-			<Icon name="Goal"/>
-			<Icon name="OwnGoal"/>
-			<Icon name="Whistle"/>
-			<Icon name="YellowCard"/>
-			<Icon name="SecondYellowCard"/>
-			<Icon name="RedCard"/>
-			<Icon name="Substitution"/>
-			<Icon name="PenaltyMissed"/>
-			<Icon name="PenaltyScored"/>
-			<Text style={styles.text}>{props.details.toString()}</Text>
+			<View style={styles.header}>
+				<Text style={styles.name}>{props.details.name}</Text>
+				<View style={styles.teams}>
+					<TeamCard details={props.details.team_data[0]} />
+					<Text style={styles.score}>{props.details.team_data[0].score} : {props.details.team_data[1].score}</Text>
+					<TeamCard details={props.details.team_data[1]} />
+				</View>
+			</View>
+			<View style={styles.body}>
+			</View>
 		</View>
 	);
 };
--- app/routes/MatchDetail/styles.js
+++ app/routes/MatchDetail/styles.js
@@ -4,13 +4,39 @@ import { colors } from '../../config/styles';
 export default StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: colors.darkish,
+	},
+	header: {
+		flex: 1,
+		backgroundColor: colors.royal,
 		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	body: {
+		flex: 2,
+		backgroundColor: colors.dark
+	},
+	teams: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-around',
 		alignItems: 'center',
-		backgroundColor: colors.positive,
+		backgroundColor: colors.darkish
 	},
 	text: {
 		fontSize: 10,
 		textAlign: 'center',
-		color: colors.royal,
+		color: colors.type,
 	},
+	score: {
+		fontSize: 20,
+		textAlign: 'center',
+		fontWeight: 'bold'
+	},
+	name: {
+		margin: 10,
+		fontSize: 20,
+		textAlign: 'center',
+		color: colors.lightish
+	}
 });
