39f17a20aba1977193bbc85164b708bb24369899
Anubhav <anubhav.dhawan@daffodilsw.com>
2017-02-02 18:28:35 +0530

    League tab

--- app/components/LeagueTab/LeagueTab.js
+++ app/components/LeagueTab/LeagueTab.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
+import moment from 'moment';
 
 const LeagueTab = props => {
 	return (
 		<View style={styles.container}>
-			<Text>League</Text>
+			<View style={styles.header}>
+				<Text style={styles.headerText}>{props.matchDetails.pool}</Text>
+				<View style={styles.subHeader}>
+					<Text style={styles.subHeaderText}>Day {props.matchDetails.match_day}</Text>
+					<Text style={styles.subHeaderText}>{`${props.matchDetails.round_name} ${props.matchDetails.round}`}</Text>
+				</View>
+			</View>
+			<View style={styles.body}>
+				<View style={styles.location}>
+					<Text style={styles.city}>{props.matchDetails.city} </Text>
+					<Text style={styles.venue}>{props.matchDetails.venue_name}</Text>
+				</View>
+				<Text style={styles.info}>{moment(props.matchDetails.local_date).format('MMMM Do YYYY, h:mm:ss a').toString()}</Text>
+				<Text style={styles.info}>Match Type: {props.matchDetails.match_type}</Text>
+				<Text style={styles.info}>Referee: {props.matchDetails.referee}</Text>
+			</View>
 		</View>
 	);
 }
--- app/components/LeagueTab/styles.js
+++ app/components/LeagueTab/styles.js
@@ -3,6 +3,46 @@ import { colors } from '../../config/styles';
 
 export default StyleSheet.create({
 	container: {
-		flex: 1
+		flex: 1,
+		padding: 10,
+		alignItems: 'center'
+	},
+	header: {
+	},
+	body: {
+		flex: 1,
+		marginTop: 10
+	},
+	headerText: {
+		fontSize: 25,
+		color: colors.royal,
+		fontWeight: 'bold',
+	},
+	subHeader: {
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	subHeaderText: {
+		color: colors.dark,
+		fontSize: 15,
+		fontWeight: 'bold'
+	},
+	info: {
+		fontWeight: 'bold',
+		color: colors.dark
+	},
+	location: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	city: {
+		fontWeight: 'bold',
+		fontSize: 20,
+		color: colors.type
+	},
+	venue: {
+		fontWeight: 'bold',
+		color: colors.dark,
+		fontSize: 15
 	}
 });
