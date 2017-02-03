d7209078c89e9c722c8c5383678e11f8ecb2ca80
Anubhav <anubhav.dhawan@daffodilsw.com>
2017-02-03 20:21:45 +0530

    Event row tyling improved

--- app/components/Event/Event.js
+++ app/components/Event/Event.js
@@ -12,33 +12,33 @@ const Event = (props) => {
 		return (
 			<View style={styles.matchEventContainer}>
 				<Icon fill={colors.dark} category={props.event.category} value={props.event.value} width="60" height="60" />
-				<Text style={styles.labelCenterText}>{`${props.event.label} `}<Text style={defaultStyles.info}>{`(${props.event.time}')`}</Text></Text>
+				<Text style={styles.labelCenterText}>{props.event.label}<Text style={defaultStyles.info}> {props.event.time}'</Text></Text>
 				<Icon fill={colors.dark} category={props.event.category} value={props.event.value} width="60" height="60" />
 			</View>
 		);
 
 	if (props.event.side === 'home') {
 		if (props.homeOnLeft) {
-			leftEventLabel = <Text style={[defaultStyles.labelText, styles.labelLeftText]}><Text style={defaultStyles.info}>{`(${props.event.time}') `}</Text>{props.event.label}</Text>;
+			leftEventLabel = <Text style={[defaultStyles.labelText, styles.labelLeftText]}><Text style={defaultStyles.info}>{props.event.time}' </Text>{props.event.label}</Text>;
 			if (props.event.additionalInfo)
-				rightEventLabel = <Text style={[defaultStyles.labelAdditionalText, styles.labelRightAdditionalText]}>{`${props.event.additionalInfo}`}</Text>;
+				rightEventLabel = <Text style={[defaultStyles.labelAdditionalText, styles.labelRightAdditionalText]}>{props.event.additionalInfo}</Text>;
 		}
 		else {
-			rightEventLabel = <Text style={[defaultStyles.labelText, styles.labelRightText]}>{props.event.label}<Text style={defaultStyles.info}>{` (${props.event.time}')`}</Text></Text>;
+			rightEventLabel = <Text style={[defaultStyles.labelText, styles.labelRightText]}>{props.event.label}<Text style={defaultStyles.info}> {props.event.time}'</Text></Text>;
 			if (props.event.additionalInfo)
-				leftEventLabel = <Text style={[defaultStyles.labelAdditionalText, styles.labelLeftAdditionalText]}>{`${props.event.additionalInfo}`}</Text>;
+				leftEventLabel = <Text style={[defaultStyles.labelAdditionalText, styles.labelLeftAdditionalText]}>{props.event.additionalInfo}</Text>;
 		}
 	}
 	else if (props.event.side === 'away') {
 		if (!props.homeOnLeft) {
-			leftEventLabel = <Text style={[defaultStyles.labelText, styles.labelLeftText]}><Text style={defaultStyles.info}>{`(${props.event.time}') `}</Text>{`${props.event.label}`}</Text>;
+			leftEventLabel = <Text style={[defaultStyles.labelText, styles.labelLeftText]}><Text style={defaultStyles.info}>{props.event.time}' </Text>{props.event.label}</Text>;
 			if (props.event.additionalInfo)
-				rightEventLabel = <Text style={[defaultStyles.labelAdditionalText, styles.labelRightAdditionalText]}>{`${props.event.additionalInfo}`}</Text>;
+				rightEventLabel = <Text style={[defaultStyles.labelAdditionalText, styles.labelRightAdditionalText]}>{props.event.additionalInfo}</Text>;
 		}
 		else {
-			rightEventLabel = <Text style={[defaultStyles.labelText, styles.labelRightText]}>{props.event.label}<Text style={defaultStyles.info}>{` (${props.event.time}')`}</Text></Text>;
+			rightEventLabel = <Text style={[defaultStyles.labelText, styles.labelRightText]}>{props.event.label}<Text style={defaultStyles.info}> {props.event.time}'</Text></Text>;
 			if (props.event.additionalInfo)
-				leftEventLabel = <Text style={[defaultStyles.labelAdditionalText, styles.labelLeftAdditionalText]}>{`${props.event.additionalInfo}`}</Text>;
+				leftEventLabel = <Text style={[defaultStyles.labelAdditionalText, styles.labelLeftAdditionalText]}>{props.event.additionalInfo}</Text>;
 		}
 	}
 	return (
