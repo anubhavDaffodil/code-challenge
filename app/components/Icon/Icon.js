8e81baeadebc7f031364464a1c318f18874dbebf
Anubhav <anubhav.dhawan@daffodilsw.com>
2017-02-03 11:48:21 +0530

    Icon proptypes

--- app/components/Icon/Icon.js
+++ app/components/Icon/Icon.js
@@ -5,22 +5,29 @@ import svgs from './svgs';
 import styles from './styles';
 
 function getIconName(category, value) {
-    switch (category) {
-        case 'period_change':
-            return 'Whistle';
-        case 'goal':
-            return value;				// Assuming value will be either Goal or OwnGoal
-        case 'substitution_on':
-            return 'SubstitutionOn';
-        case 'substitution_off':
-            return 'SubstitutionOff';
-        case 'booking':
-            return `${value}Card`;		// Assuming value will be 'Yellow', 'Red', or 'SecondYellow'
-        case 'penaltyshot':
-            return `Penalty${value}`	// Assuming value will be 'Scored', 'Saved', or 'Missed'
-        default:
-            return value;
-    }
+	switch (category) {
+		case 'period_change':
+			return 'Whistle';
+		case 'goal':
+			return value;				// Assuming value will be either Goal or OwnGoal
+		case 'substitution_on':
+			return 'SubstitutionOn';
+		case 'substitution_off':
+			return 'SubstitutionOff';
+		case 'booking':
+			return `${value}Card`;		// Assuming value will be 'Yellow', 'Red', or 'SecondYellow'
+		case 'penaltyshot':
+			return `Penalty${value}`	// Assuming value will be 'Scored', 'Saved', or 'Missed'
+		default:
+			return value;
+	}
 }
 
-export default (props) => <View style={styles.container}><SvgIcon {...props} name={getIconName(props.category, props.value)} svgs={svgs} /></View>;
+const Icon = props => <View style={styles.container}><SvgIcon {...props} name={getIconName(props.category, props.value)} svgs={svgs} /></View>;
+
+Icon.propTypes = {
+	name: React.PropTypes.string,
+	category: React.PropTypes.string
+};
+
+export default Icon;
